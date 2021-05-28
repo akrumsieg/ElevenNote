714cc53a5147e04d5a07b12a2842c72a3b4da368
Aaron Krumsieg <a.krumsieg@gmail.com>
2021-05-28 08:14:50 -0500

    feat: in .Data: Add Category class

--- ElevenNote.Data/Note.cs
+++ ElevenNote.Data/Note.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -11,12 +12,22 @@ namespace ElevenNote.Data
     {
         [Key]
         public int NoteId { get; set; }
+
+        [Required]
+        public string CategoryName { get; set; }
+        [ForeignKey(nameof(CategoryName))]
+        public virtual Category Category { get; set; }
+
         [Required]
         public Guid OwnerId { get; set; }
+
         [Required]
         public string Title { get; set; }
+
         [Required]
         public string Content { get; set; }
+
+
         [Required]
         public DateTimeOffset CreatedUtc { get; set; }
         public DateTimeOffset? ModifiedUtc { get; set; }
